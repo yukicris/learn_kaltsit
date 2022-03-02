92aae811e57d55eebad2b1ef563e58ee0f90bfe5
yukicris <yewei2008com@yeah.net>
2022-03-02 14:28:47 +0800

    原始项目初始化

---
+++ src/main/java/com/yukicris/learn_write/LearnWriteApplication.java
@@ -0,0 +1,13 @@
+package com.yukicris.learn_write;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnWriteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yukicris/learn_write/LearnWriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukicris.learn_write;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWriteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
